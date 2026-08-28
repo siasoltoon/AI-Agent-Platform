6a51eefc7bb6011bd7b1caacbe956b0f1b04893d
siasoltoon <siyavashjahangiri105@gmail.com>
2026-08-29 00:16:37 +0330

    Add tool registry

---
+++ tool_system/tool_registry.py
@@ -0,0 +1,9 @@
+class ToolRegistry:
+    def __init__(self):
+        self.tools = {}
+
+    def register(self, tool):
+        self.tools[tool.name] = tool
+
+    def get(self, name):
+        return self.tools.get(name)
