f83ef287bfcde4769c6d60656424a64c10e961df
siasoltoon <siyavashjahangiri105@gmail.com>
2026-08-29 00:14:43 +0330

    Add Agent memory layer

---
+++ agent_core/memory.py
@@ -0,0 +1,9 @@
+class AgentMemory:
+    def __init__(self):
+        self.history = []
+
+    def remember(self, item):
+        self.history.append(item)
+
+    def get_history(self):
+        return self.history
